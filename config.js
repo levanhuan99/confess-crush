6f04cd4c980121b87a5f4d2b48e6279da54bcda2
levanhuan99 <61698248+levanhuan99@users.noreply.github.com>
2024-02-13 08:18:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Diệu Diệu",
+    introTitle: 'Diệu Diệu',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
