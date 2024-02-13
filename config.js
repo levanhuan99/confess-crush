d26cf96839ff2964325066911013a97a17c5d3e2
levanhuan99 <61698248+levanhuan99@users.noreply.github.com>
2024-02-13 08:30:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,15 +6,15 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    title: 'Phải chăng cậu đã yêu nửa kia ngay từ cái nhìn đầu tiên 🥰.Nếu vậy cậu nên mua socola tặng cho 1 nửa yêu thương đi nhé.',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Có rất nhìu người cũng có socola mà bạn lại chọn của mình??? <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'Mình biết mà 🥰. Vì nó rất đẹp đúng khum 😘😘',
+    messDesc: 'Tối nay 7h, mình gửi cậu nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
